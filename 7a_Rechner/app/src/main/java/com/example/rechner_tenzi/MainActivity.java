88787ec2a4a342348d11881c47d8228b17b8cdeb
tenzi <tenzi@student.tgm.ac.at>
2023-09-13 11:16:03 +0200

    OnClick-Listener für Berechnen Button gemacht, onTouch-Listener für das Ergebnisfeld

--- 7a_Rechner/app/src/main/java/com/example/rechner_tenzi/MainActivity.java
+++ 7a_Rechner/app/src/main/java/com/example/rechner_tenzi/MainActivity.java
@@ -3,6 +3,12 @@ package com.example.rechner_tenzi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +16,54 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView result = findViewById(R.id.textview_result);
+        result.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                result.setText(0);
+                return true;
+            }
+        });
+
     }
+
+    public void calculate(View view) {
+        EditText input1 = view.findViewById(R.id.edit_wert1);
+        int value1 = Integer.parseInt(input1.getText().toString());
+
+        EditText input2 = view.findViewById(R.id.edit_wert2);
+        int value2 = Integer.parseInt(input2.getText().toString());
+
+        RadioGroup radioGroup = view.findViewById(R.id.radioGroup);
+        int radioButtonId = radioGroup.getCheckedRadioButtonId();
+        RadioButton radioButton = findViewById(radioButtonId);
+
+        TextView result = view.findViewById(R.id.textview_result);
+
+        switch(radioButton.getText().toString()) {
+            case "+":
+                result.setText(value1+value2);
+                break;
+            case "-":
+                result.setText(value1-value2);
+                break;
+            case "*":
+                result.setText(value1*value2);
+                break;
+            case "/":
+                if(value2==0) {
+                    result.setText("Error");
+                }
+                else {
+                    result.setText(value1/value2);
+                }
+                break;
+        }
+
+
+
+    }
+
 
 }
