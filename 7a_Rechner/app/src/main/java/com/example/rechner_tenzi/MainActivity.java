a517d543b00e17f83ca20e6ec0bf82d8c675e84c
tenzi <tenzi@student.tgm.ac.at>
2023-09-14 15:55:40 +0200

    Speicher- und Lesemethoden implementiert. Farbänderung des Buttons implementiert.

--- 7a_Rechner/app/src/main/java/com/example/rechner_tenzi/MainActivity.java
+++ 7a_Rechner/app/src/main/java/com/example/rechner_tenzi/MainActivity.java
@@ -2,15 +2,26 @@ package com.example.rechner_tenzi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Button calculate = findViewById(R.id.button_calc);
+        calculate.setBackgroundColor(Color.GREEN);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +39,7 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+
     public void calculate(View view) {
         EditText input1 = view.findViewById(R.id.edit_wert1);
         int value1 = Integer.parseInt(input1.getText().toString());
@@ -41,29 +53,62 @@ public class MainActivity extends AppCompatActivity {
 
         TextView result = view.findViewById(R.id.textview_result);
 
+        int result_value = 0;
+
         switch(radioButton.getText().toString()) {
             case "+":
-                result.setText(value1+value2);
+                result_value = value1+value2;
                 break;
             case "-":
-                result.setText(value1-value2);
+                result_value = value1-value2;
                 break;
             case "*":
-                result.setText(value1*value2);
+                result_value = value1*value2;
                 break;
             case "/":
                 if(value2==0) {
                     result.setText("Error");
                 }
                 else {
-                    result.setText(value1/value2);
+                    result_value = value1/value2;
                 }
                 break;
         }
+        if(result_value<0) {
+            result.setTextColor(Color.RED);
+        }
+        else {
+            result.setTextColor(Color.BLACK);
+        }
+        result.setText(result_value);
 
 
 
     }
 
+    public void saveValues(View view) {
+        EditText input1 = view.findViewById(R.id.edit_wert1);
+        int value1 = Integer.parseInt(input1.getText().toString());
+
+        EditText input2 = view.findViewById(R.id.edit_wert2);
+        int value2 = Integer.parseInt(input2.getText().toString());
+
+        SharedPreferences sharedPreferences = this.getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putInt("value1",value1);
+        editor.putInt("value2",value2);
+        editor.commit();
+    }
+    public void readValues(View view) {
+        SharedPreferences sharedPreferences = this.getPreferences(Context.MODE_PRIVATE);
+
+        EditText input1 = view.findViewById(R.id.edit_wert1);
+        int value1 = sharedPreferences.getInt("value1",0);
+        input1.setText(value1);
+
+        EditText input2 = view.findViewById(R.id.edit_wert2);
+        int value2 = sharedPreferences.getInt("value2",0);
+        input2.setText(value2);
+    }
 
 }
